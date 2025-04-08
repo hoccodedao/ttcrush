224605ba9970dda2d989a172f2e94251b5ff0750
hoccodedao <chillwith01@gmail.com>
2025-04-08 15:27:43 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "To tinh ca rot",
+    introTitle: 'Cậu ơiii',
+    introDesc: `trái đất có 7 tỉ người không hiểu sao trái tim tôi
+    lại lạc vào ánh mắt cậu rùi`,
+    btnIntro: '...',
+    title: 'Tuy nhà tui nghèo không có điều kiện lo cho cậu',
+    desc: 'nhưng tui lại lỡ thích cậu mất rùii bạn có thể cho tui cơ hội không !!',
+    btnYes: 'không ! làm gì có cơ hội nào',
+    btnNo: 'oce ! tớ đồng ý ',
+    question: 'Sao bạn lại từ chối tình cảm của tui vậy',
+    btnReply: 'Gửi Câu Trả Lời',
+    reply: 'Cậu đã nghèo còn xấu nữa thì làm gì xứng đáng có tình yêu chứ đúng không hahahahaaaaa',
+    mess: 'Mình biết mà ! mình sẽ thay đổi để hoàn thiện hơn',
+    messDesc: 'cảm ơn bạn đã nhắc nhở ',
+    btnAccept: 'Khong sao ! co gang thay doi ban than de hoan thien hon nha',
+}
